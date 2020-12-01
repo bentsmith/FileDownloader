ae92da0bcc0365cd9308d60637023c79aabb8e47
Ben Smith <benjamin.t.smith1@maine.edu>
2020-12-01 12:14:26 -0500

    need to fix so it downloads all files, not just with extension

--- FileDownloader.py
+++ FileDownloader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env Python
 
 import requests
-import re
+import os
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
@@ -10,19 +10,25 @@ headers = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple WebKit/537.36 (KHTML, like Gecko) Chrome970.0.3538.77 Safari/537.36"
 }
 
+Url = "https://www.stoningtonschools.org/board-of-ed/meeting-minutes"
+folder_location = os.getcwd()
+#if not os.path.exists(folder_location):os.mkdir(folder_location)
+
 def getLinks(URL):
 	response = requests.get(URL, headers=headers, timeout=5)
 	page = response.text
 	soup = BeautifulSoup(page, 'html.parser')
 
 	# get main content of page, so we don't parse through navigation and footer links
-	mainContent= soup.find('main')
-
-	for link in mainContent.findAll('a', attrs={'href': re.compile("^https?://")}):
-
+	mainContent = soup.find('main')
+		#findAll('a', attrs={'href': re.compile("^https?://")}):
+	for link in mainContent.select("a[href]"):
+		filename = os.path.join(folder_location,link['href'].split('/')[-1])
+		with open(filename, 'wb') as f:
+			f.write(requests.get(urljoin(Url,link['href'])).content)
 
 def main():
-	URL = "https://www.stoningtonschools.org/board-of-ed/meeting-minutes"
+	getLinks(Url)
 
 
 if __name__ == '__main__':
